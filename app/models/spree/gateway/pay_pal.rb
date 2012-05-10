3412e9d8a8f09efa154494ec7996da68e6ecb6fc
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2012-05-10 17:57:39 +0100

    making paypal gateway compatible with active merchant

--- app/models/spree/gateway/pay_pal.rb
+++ app/models/spree/gateway/pay_pal.rb
@@ -3,9 +3,9 @@ module Spree
     preference :login, :string
     preference :password, :string
     preference :signature, :string
-    preference :currency_code, :string
+    preference :currency, :string
     
-    attr_accessible :preferred_login, :preferred_password, :preferred_signature, :preferred_currency_code
+    attr_accessible :preferred_login, :preferred_password, :preferred_signature, :preferred_currency
 
     def provider_class
       ActiveMerchant::Billing::PaypalGateway
